76affb3e5ac484a7356e0a0b2849375f8ac0edc5
AnnaPS <anna-91_@hotmail.com>
2020-05-30 14:33:32 +0200

    use random color libray to change the background color when click the button

--- lib/ball.dart
+++ lib/ball.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:random_color/random_color.dart';
 
 class Ball extends StatefulWidget {
   @override
@@ -8,25 +9,16 @@ class Ball extends StatefulWidget {
 
 class _BallState extends State<Ball> {
   int ballNumber = 1;
-  static const colorA = 255; // Transparency (Alpha)
-  int colorR, colorG, colorB = 127;
-  int colorBackground = 4286545791; // Initial grey background
   @override
   Widget build(BuildContext context) {
+    RandomColor _randomColor = RandomColor();
     return Container(
-      color: Color(colorBackground),
+      color: _randomColor.randomColor(),
       child: Center(
         child: FlatButton(
           onPressed: () {
             setState(() {
-              ballNumber = Random(DateTime.now().millisecond).nextInt(5) +
-                  1; // Gives a more random seed to the Random function
-              colorR = Random().nextInt(26) *
-                  10; // could have used Random().nextInt(256);
-              colorG = Random().nextInt(26) * 10;
-              colorB = Random().nextInt(26) * 10;
-              colorBackground =
-                  (((((colorA * 256) + colorR) * 256) + colorG) * 256) + colorB;
+              ballNumber = Random(DateTime.now().millisecond).nextInt(5) + 1;
             });
           },
           child: Image.asset("images/ball$ballNumber.png"),
